ac158ec67fb9ea44d9a3c190deb0a1dc4ab2f674
i8hamburgrz <sbrand87@gmail.com>
2020-02-22 17:23:24 -0800

    add new endpoint to get search results

--- src/store/api.js
+++ src/store/api.js
@@ -1,6 +1,12 @@
 import request from "./request";
 
 export const searchSuggestions = (query) => {
+  // we only want to return 5 results here, we do not need all results
   const url = `https://api.github.com/search/issues?q=${query}+in:react+user:facebook&per_page=5`;
+  return request(url)
+}
+
+export const searchIssues = (query) => {
+  const url = `https://api.github.com/search/issues?q=${query}+in:react+user:facebook`;
   return request(url)
 }
