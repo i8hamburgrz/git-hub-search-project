1435de35301495445dba6c10cf6ec7c2e3990df8
i8hamburgrz <sbrand87@gmail.com>
2020-02-22 20:27:52 -0800

    remove stuff

--- src/store/request.js
+++ src/store/request.js
@@ -1,7 +1,7 @@
 function request(url, headers = {
   method: "GET",
   accept: "application/vnd.github.v3.text-match+json",
-  authorization: "token 500b2b9a69a4450006dcab6c70b9fab90aaffeaf"
+  // authorization: "token 500b2b9a69a4450006dcab6c70b9fab90aaffeaf"
 }) {
   return fetch(url, headers)
     .then(response => {
